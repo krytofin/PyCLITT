59ffa587b7f1ee1d40424208833d30c1a79a2870
krytofin <kzn.dragons.craft@gmail.com>
2024-08-18 21:27:59 +0300

    print list tasks function to task manager

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import json
 import sys
 
 
+
+
 class Task:
     task_id = 0
 
+    
     @classmethod
     def __increment_id(cls) -> None:
         cls.task_id += 1
@@ -37,6 +40,10 @@ class Task:
         self.__class__.task_id = self._id
         self.__increment_id()
 
+    @property
+    def id(self):
+        return self._id
+    
     def save(self):
         try:
             with open("store/tasks.json", '+r') as json_file:
@@ -55,23 +62,37 @@ class Task:
         with open("store/tasks.json", '+w') as json_file: 
             json.dump(data, json_file, indent=4)
 
-    def delete(self): ...
-
-    def update(self): ...
-
-    def get(self): ...
-
 
 class Tracker:
+    
+    class Options:
+        all = 'all'
+        done = 'done'
+        todo = 'todo'
+        int_progress = 'in-progress'
+        
     def __init__(self) -> None:
         self._tasks = {}
-        with open("store/tasks.json") as json_file:
-            json_file = json.load(json_file)
-        for task in json_file:
-            self._tasks[task["id"]] = Task(task=task)
-
-        print(self._tasks)
-
+        try:
+            with open("store/tasks.json") as json_file:
+                json_file = json.load(json_file)
+            for task in json_file:
+                self._tasks[task["id"]] = Task(task=task)
+        except json.decoder.JSONDecodeError:
+            # tasks list is empty
+            pass
+        
+    def add(self, description):
+        new_task = Task(new=True, description=description)
+        task_id = new_task.id
+        self._tasks[task_id] = new_task
+        new_task.save()
+        print(f'[+] Task added successfully {new_task}')
+        
+    def print_list(self, option=Options.all):
+        if option == self.Options.all:
+            for index, task in self._tasks.items():
+                print(index, task)
 
 def show_avalible_flags() -> None:
     print("Possible flags:\n")
@@ -86,15 +107,14 @@ def show_avalible_flags() -> None:
     print("\tlist <str:done> - to print all tasks with status done")
     print("\tlist <str:todo> - to print all tasks with status todo")
     print("\tlist <str:in-progress> - to print all tasks with status in-progress")
-    print("\t-h - to print this help manual")
+    print("\thelp - to print this help manual")
 
 
 if __name__ == "__main__":
     task_manager = Tracker()
-    for _ in range(10):
-        task = Task(new=True, description='Test task')
-        task.save()
-    
+
+    # ==============================
+    # Flags
     arhv_len = len(sys.argv)
     if arhv_len < 2:
         print("The flag was expected to be entered")
@@ -106,24 +126,28 @@ if __name__ == "__main__":
             case "add":
                 if arhv_len > 2:
                     description = " ".join(sys.argv[2:])
-                    print(description)
+                    task_manager.add(description)
                 else:
                     print("[-] excepted description as third argument")
 
             case "update":
                 print("update task")
+                
             case "delete":
                 print("delete task")
+                
             case "mark-in-progress":
                 print("replace to mark-in-progress")
+                
             case "mark-done":
                 print("mark-done")
+                
             case "list":
                 if arhv_len > 2:
                     ...
                 else:
-                    print("list")
-            case "-h":
+                    task_manager.print_list()
+            case "help":
                 show_avalible_flags()
             case _:
                 print("The flag was expected to be entered")
