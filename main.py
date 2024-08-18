b10ec35de1cbe54cbdc66c4631a89b30b12379e7
krytofin <kzn.dragons.craft@gmail.com>
2024-08-18 19:04:00 +0300

    add init function to Task class

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import datetime
+
+
 class Task:
-    ...
-    
-class Tracker:
-    ...
+    task_id = 0
+
+    @classmethod
+    def __increment_id(cls):
+        cls.task_id += 1
+
+    def __init__(self, description, status="todo") -> None:
+        self._id = self.task_id
+        self._description = description
+        self._status = status
+        self._createdAt = datetime.datetime.now()
+        self._updatedAt = datetime.datetime.now()
+
+        self.__increment_id()
+
+
+class Tracker: ...
